ea9e1520b6dcece67572edf79d629e9fb3f79a7b
Abdulloh <boxodirovabdulloh46@gmail.com>
2026-01-17 20:42:02 +0500

    mana na xxxxuyyy

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav>
+        <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar
