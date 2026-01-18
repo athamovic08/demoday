1f4f53d45f63bbf9653c1adcb74ee30eb7477440
SAYKO <saidalomirsabitov@gmail.com>
2026-01-18 14:25:39 +0500

    qoto

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Route , Routes } from 'react-router-dom'
 import Home from './Pages/Home'
 import ProductPage from './Pages/Product'
-
+import ProfilePage from './Components/Profile'
 const App = () => {
   return (
     <div>
@@ -13,7 +13,8 @@ const App = () => {
       <Routes>
          <Route path='/' element={<Home/>}/>
          <Route path="/product/:id" element={<ProductPage/>}/>
-        
+         <Route path="/profile" element={<ProfilePage/>}/>
+       
       </Routes>
     </div>
   )
